9f620fe191d07b42053fd9f42474dac58710cd6a
Michel <kintelalpz@gmail.com>
2021-11-27 19:16:32 +0100

    modification police table

--- server.js
+++ server.js
@@ -18,37 +18,41 @@ var arduino = new SerialPort(usbserial, { autoOpen: false });
 // Requetes: traitement communication entre HTML et Server
 
 io.sockets.on('connection', function (socket) {
-  // Message à la connection
-  console.log('Connexion socket : Ok');
-  socket.emit('message', 'Connexion : Ok\n');   // envoi vers html
-  // Le serveur reçoit un message du navigateur    
-  socket.on('message', function (msg) {
-    console.log('reçu de html: ' + msg);        // impression message reçu de html
-    socket.emit('message', 'commande envoyé au serveur: ' + msg);
 
-    arduino.write(msg, function (err) {   // envoi vers arduino
-      if (err) {
-        io.sockets.emit('message', err.message);
-        return console.log('Error: ', err.message);
-      }
-    });
-  });
-  socket.on('port', function (port) {   // TODO
-    usbserial = port;
-    console.log("port sélectionné: " + usbserial);
+          // Message à la connection
+          console.log('Connexion socket : Ok');
 
-    // Overture du port serie
-    arduino.open(function (err) {
-      if (err) {
-        return console.log('Error opening port: ', err.message);
-      }
-      else {
-        console.log("Communication serie Arduino 9600 bauds : Ok")
-      }
-    });
+          socket.emit('message', 'Connexion : Ok\n');   // envoi vers html
+          // Le serveur reçoit un message du navigateur    
+
+          socket.on('message', function (msg) {
+                console.log('reçu de html: ' + msg);        // impression message reçu de html
+                socket.emit('message', 'commande envoyé au serveur: ' + msg);
+
+                arduino.write(msg, function (err) {   // envoi vers arduino
+                      if (err) {
+                        io.sockets.emit('message', err.message);
+                        return console.log('Error: ', err.message);
+                      }
+                });
+          });
+
+          socket.on('port', function (port) {   // TODO
+                usbserial = port;
+                console.log("port sélectionné: " + usbserial);
+
+                // Overture du port serie
+                arduino.open(function (err) {
+                      if (err) {
+                        return console.log('Error opening port: ', err.message);
+                      }
+                      else {
+                        console.log("Communication serie Arduino 9600 bauds : Ok")
+                      }
+                });
 
 
-  })
+          })
 });
 
 // Communication entre Arduino et Serveur
